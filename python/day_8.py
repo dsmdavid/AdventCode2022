f3c84761ca97d5fe20d7623a30eb5d48adcc30a8
dsmdavid <61737063+dsmdavidIL@users.noreply.github.com>
2022-12-08 21:23:15 +0000

    day 8

---
+++ python/day_8.py
@@ -0,0 +1,118 @@
+import os
+from collections import defaultdict
+import pprint
+from copy import deepcopy
+import sys
+
+SAMPLE_DATA = """30373
+25512
+65332
+33549
+35390"""
+
+SUMS = []
+
+
+def get_input(fn):
+    if not fn:
+        return SAMPLE_DATA.strip()
+    with open(fn, "r") as f:
+        return f.read().strip()
+
+
+class ForestTree:
+    def __init__(self, height, row_index, col_index, row, col) -> None:
+        self.height = height
+        self.row_index = row_index
+        self.col_index = col_index
+        self.row = row
+        self.col = col
+        self.is_visible = ""
+
+    def return_trees_fragment(self, index, fragment, order):
+        ct = 0
+        test_fragment = fragment[0:index] if order == 1 else fragment[0:index][::-1]
+
+        for i in test_fragment:
+            if i < self.height:
+                ct += 1
+            elif i >= self.height:
+                ct += 1
+                return ct
+            else:
+                return ct
+        return ct
+
+    # def print_me(self):
+    #     print(self.row_index, self.col_index, self.height, self.row, self.col)
+    def max_visibility(self):
+        n, e, s, w = 0, 0, 0, 0
+        # borders
+        if self.col_index == 0 or self.col_index == len(self.row) - 1:
+            return 0
+        elif self.row_index == 0 or self.row_index == len(self.col) - 1:
+            return 0
+        else:
+            pass
+        n = self.return_trees_fragment(self.row_index, self.col, order=-1)
+        e = self.return_trees_fragment(self.col_index, self.row, order=-1)
+        s = self.return_trees_fragment(
+            len(self.row) - self.row_index - 1, self.col[::-1], order=-1
+        )
+        w = self.return_trees_fragment(
+            len(self.col) - self.col_index - 1, self.row[::-1], order=-1
+        )
+        return n * e * s * w
+
+    def set_visible(self):
+        self.is_visible = self.is_tree_visible()
+
+    def is_tree_visible(self):
+        try:
+            if self.col_index == 0 or self.col_index == len(self.row) - 1:
+                return True
+            elif self.row_index == 0 or self.row_index == len(self.col) - 1:
+                return True
+            elif max(self.row[0 : self.col_index]) < self.height:
+                return True
+            elif max(self.row[self.col_index + 1 :]) < self.height:
+                return True
+            elif max(self.col[0 : self.row_index]) < self.height:
+                return True
+            elif max(self.col[self.row_index + 1 :]) < self.height:
+                return True
+            else:
+                return False
+        except:
+            print(self.__dict__)
+            raise
+
+
+if __name__ == "__main__":
+    input = get_input(os.path.join("./inputs", "2022__8.txt"))
+    # input = get_input(None)
+    raw_rows = input.split("\n")
+    rows = []
+    columns = []
+    for i, row in enumerate(raw_rows):
+        rows.append(list(map(int, list(row))))
+    for i in range(len(rows[0])):
+        _ = [r[i] for r in rows]
+        columns.append(_)
+    forest_trees = []
+    # create trees and set visibility from outside
+    for i, row in enumerate(rows):
+        for k, item in enumerate(row):
+            FT = ForestTree(
+                height=item, row_index=i, col_index=k, row=row, col=columns[k]
+            )
+            FT.set_visible()
+            forest_trees.append(FT)
+
+    ct = 0
+    mx = 0
+    for tree in forest_trees:
+        mx = max(mx, tree.max_visibility())
+        if tree.is_visible:
+            ct += 1
+    print("part1\t", ct, "\npart2\t", mx)
---
+++ sql/day_8.sql
@@ -0,0 +1,320 @@
+-- create base file
+-- create or replace table TEST_DB.AOC2022.INPUT_2022_08 as
+-- select t.$1 as input,
+--     metadata$filename as fn,
+--     metadata$file_row_number as rn
+-- from @stage_aoc (pattern => '.*2022__8.txt.gz') as t;
+-- select * from TEST_DB.AOC2022.INPUT_2022_08;
+create or replace table TEST_DB.AOC2022.solved_day_8 as with sample_values as (
+                    select '30373' as input, 1 as rn
+        union all   select '25512', 2
+        union all   select '65332', 3
+        union all   select '33549', 4
+        union all   select '35390', 5
+    ),
+    source as (
+        select *
+        from input_2022_08 
+            -- sample_values
+    ),
+    narrow as (
+        -- split(regexp_replace(i.input, '.', ',\\0', 2), ',') as split_input,
+        -- used for flattenning
+        select i.rn,
+            f.value::number as height,
+            f.index + 1 as cn
+        from source as i,
+            lateral flatten(
+                input => split(regexp_replace(i.input, '.', ',\\0', 2), ',')
+            ) as f
+    ),
+    directions as (
+                  select 'up' as dir
+        union all select 'down'
+        union all select 'left'
+        union all select 'right'
+    ),
+    sample_ as (
+        select narrow.*,
+            directions.dir
+        from narrow
+            cross join directions 
+            --where rn= 2 and cn =4
+    ),
+    rec as (
+        select sample_.rn,
+            sample_.cn,
+            sample_.height,
+            0 as ct,
+            sample_.dir,
+            sample_.rn as n_rn,
+            sample_.cn as n_cn,
+            1 as continue,
+            0 as n_height
+        from sample_
+        union all
+        select rec.rn,
+            rec.cn,
+            rec.height,
+            rec.ct + 1 as ct,
+            rec.dir,
+            n.rn as n_rn,
+            n.cn as n_cn,
+            case
+                when rec.height <= n.height then 0
+                else 1
+            end,
+            n.height as n_height
+        from rec
+            inner join narrow as n on (
+                case
+                    when rec.dir = 'up' then rec.cn = n.cn
+                    and rec.n_rn - 1 = n.rn
+                    when rec.dir = 'down' then rec.cn = n.cn
+                    and rec.n_rn + 1 = n.rn
+                    when rec.dir = 'left' then rec.n_cn - 1 = n.cn
+                    and rec.rn = n.rn
+                    when rec.dir = 'right' then rec.n_cn + 1 = n.cn
+                    and rec.rn = n.rn
+                end
+            )
+        where 1 = 1 
+            and rec.continue = 1
+    ),
+    seen_by as (
+        select n.rn,
+            n.cn,
+            n.height,
+            min(
+                case
+                    when rec.dir = 'up' then rec.n_rn
+                end
+            ) as ups,
+            max(
+                case
+                    when rec.dir = 'down' then rec.n_rn
+                end
+            ) as downs,
+            min(
+                case
+                    when rec.dir = 'left' then rec.n_cn
+                end
+            ) as lefts,
+            max(
+                case
+                    when rec.dir = 'right' then rec.n_cn
+                end
+            ) as rights,
+            case
+                when ups = 1 then 1
+                when downs = (
+                    select max(rn)
+                    from narrow
+                ) then 1
+                when lefts = 1 then 1
+                when rights = (
+                    select max(cn)
+                    from narrow
+                ) then 1
+                else 0
+            end as is_visible
+        from sample_ as n
+            left join rec on n.rn = rec.rn
+            and n.cn = rec.cn
+        where rec.continue = 1
+        group by 1,
+            2,
+            3
+    ),
+    all_visibility as (
+        select n.rn,
+            n.cn,
+            n.height,
+            max(
+                case
+                    when rec.dir = 'up' then rec.ct
+                end
+            ) as ups,
+            max(
+                case
+                    when rec.dir = 'down' then rec.ct
+                end
+            ) as downs,
+            max(
+                case
+                    when rec.dir = 'left' then rec.ct
+                end
+            ) as lefts,
+            max(
+                case
+                    when rec.dir = 'right' then rec.ct
+                end
+            ) as rights,
+            ups * downs * lefts * rights as visibility
+        from sample_ as n
+            left join rec on n.rn = rec.rn
+            and n.cn = rec.cn --where rec.continue = 1
+        group by 1,
+            2,
+            3
+    )
+select 'part1' as part,
+    count(*) as answer
+from seen_by
+where is_visible = 1
+union all
+select 'part2' as part,
+    max(visibility) as answer
+from all_visibility;
+
+
+/* split into 4 (up, down, left, right) seem to be more performant
+ 
+ with sample_values as (
+ select '30373' as input, 1 as rn
+ union all select '25512',2
+ union all select '65332',3
+ union all select '33549',4
+ union all select '35390',5
+ ),
+ source as (
+ select * from
+ input_2022_08
+ -- sample_values
+ ),
+ narrow as (    
+ select 
+ i.rn,
+ -- split(regexp_replace(i.input, '.', ',\\0', 2), ',') as split_input,
+ f.value::number as height,
+ f.index + 1 as cn
+ 
+ from source as i,
+ lateral flatten(input=>split(regexp_replace(i.input, '.', ',\\0', 2), ',')) as f
+ ),
+ sample_ as (
+ select * from narrow 
+ --where rn= 2 and cn =4
+ ),
+ ups as (
+ select sample_.rn, sample_.cn, sample_.height, 0 as ct, sample_.rn as n_rn, sample_.cn as n_cn, 1 as continue, 0 as n_height
+ from sample_
+ union all 
+ select rec.rn, rec.cn, rec.height, rec.ct +1 as ct, n.rn as n_rn, n.cn as n_cn,
+ case when rec.height <= n.height then 0 else 1 end, n.height as n_height
+ from ups as rec
+ inner join narrow as n 
+ on rec.cn = n.cn
+ and rec.n_rn - 1 = n.rn
+ where 1=1 --ups.height >= n.height
+ and rec.continue = 1
+ ),
+ downs as (
+ select sample_.rn, sample_.cn, sample_.height, 0 as ct, sample_.rn as n_rn, sample_.cn as n_cn, 1 as continue, 0 as n_height
+ from sample_
+ union all 
+ select rec.rn, rec.cn, rec.height, rec.ct +1 as ct, n.rn as n_rn, n.cn as n_cn,
+ case when rec.height <= n.height then 0 else 1 end, n.height as n_height
+ from downs as rec
+ inner join narrow as n 
+ on rec.cn = n.cn
+ and rec.n_rn + 1 = n.rn
+ where 1=1 --ups.height >= n.height
+ and rec.continue = 1
+ ),
+ lefts as (
+ select sample_.rn, sample_.cn, sample_.height, 0 as ct, sample_.rn as n_rn, sample_.cn as n_cn, 1 as continue, 0 as n_height
+ from sample_
+ union all 
+ select rec.rn, rec.cn, rec.height, rec.ct +1 as ct, n.rn as n_rn, n.cn as n_cn,
+ case when rec.height <= n.height then 0 else 1 end, n.height as n_height
+ from lefts as rec
+ inner join narrow as n 
+ on rec.n_cn - 1 = n.cn
+ and rec.rn = n.rn
+ where 1=1 --ups.height >= n.height
+ and rec.continue = 1
+ ),
+ rights as (
+ select sample_.rn, sample_.cn, sample_.height, 0 as ct, sample_.rn as n_rn, sample_.cn as n_cn, 1 as continue, 0 as n_height
+ from sample_
+ union all 
+ select rec.rn, rec.cn, rec.height, rec.ct +1 as ct, n.rn as n_rn, n.cn as n_cn,
+ case when rec.height <= n.height then 0 else 1 end, n.height as n_height
+ from rights as rec
+ inner join narrow as n 
+ on rec.n_cn + 1 = n.cn
+ and rec.rn = n.rn
+ where 1=1 --ups.height >= n.height
+ and rec.continue = 1
+ ),
+ all_visibility as (
+ select 
+ n.rn,
+ n.cn,
+ n.height,
+ max(ups.ct) as ups,
+ max(downs.ct) as downs,
+ max(lefts.ct) as lefts,
+ max(rights.ct) as rights,
+ ups * downs * lefts * rights as visibility
+ from sample_ as n
+ left join ups 
+ on n.rn = ups.rn
+ and n.cn = ups.cn
+ left join downs 
+ on n.rn = downs.rn
+ and n.cn = downs.cn
+ left join lefts 
+ on n.rn = lefts.rn
+ and n.cn = lefts.cn
+ left join rights 
+ on n.rn = rights.rn
+ and n.cn = rights.cn
+ group by 1,2,3
+ ),
+ seen_by as (
+ select 
+ n.rn,
+ n.cn,
+ n.height,
+ min(ups.n_rn) as ups,
+ max(downs.n_rn) as downs,
+ min(lefts.n_cn) as lefts,
+ max(rights.n_cn) as rights,
+ case 
+ when ups = 1 then 1
+ when downs = (select max(rn) from narrow) then 1
+ when lefts = 1 then 1
+ when rights = (select max(cn) from narrow) then 1
+ else 0 end as is_visible
+ 
+ from sample_ as n
+ left join ups 
+ on n.rn = ups.rn
+ and n.cn = ups.cn
+ left join downs 
+ on n.rn = downs.rn
+ and n.cn = downs.cn
+ left join lefts 
+ on n.rn = lefts.rn
+ and n.cn = lefts.cn
+ left join rights 
+ on n.rn = rights.rn
+ and n.cn = rights.cn
+ where ups.continue = 1
+ and downs.continue = 1
+ and lefts.continue = 1
+ and rights.continue = 1
+ group by 1,2,3
+ )
+ 
+ select 'part1' as part, count(*) as answer from seen_by
+ where is_visible = 1
+ union all
+ select 
+ 'part2' as part, 
+ max(visibility) as answer
+ from all_visibility
+ ;
+ */
