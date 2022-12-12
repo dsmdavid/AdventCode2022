9fb372832891e8f96a87acf69b47e7ccb6c979ce
David Sanchez <dsmdavid@gmail.com>
2022-12-12 22:11:23 +0000

    Update day_11.sql

--- sql/day_11.sql
+++ sql/day_11.sql
@@ -4,244 +4,121 @@ select t.$1 as input,
     metadata$file_row_number as rn
 from @stage_aoc (pattern => '.*2022__11.txt.gz') as t;
 
-create or replace table TEST_DB.AOC2022.solved_2022_10 as
+/* close, but no bananas */ 
+-- the below code is WIP and not working
 
-with 
--- sample_data_a as (
---     select 'noop' as input, 1 as rn
---     union all select 'addx 3',2
---     union all select 'addx -5',3
--- ),
--- sample_input_raw as (
--- select $$addx 15
--- addx -11
--- addx 6
--- addx -3
--- addx 5
--- addx -1
--- addx -8
--- addx 13
--- addx 4
--- noop
--- addx -1
--- addx 5
--- addx -1
--- addx 5
--- addx -1
--- addx 5
--- addx -1
--- addx 5
--- addx -1
--- addx -35
--- addx 1
--- addx 24
--- addx -19
--- addx 1
--- addx 16
--- addx -11
--- noop
--- noop
--- addx 21
--- addx -15
--- noop
--- noop
--- addx -3
--- addx 9
--- addx 1
--- addx -3
--- addx 8
--- addx 1
--- addx 5
--- noop
--- noop
--- noop
--- noop
--- noop
--- addx -36
--- noop
--- addx 1
--- addx 7
--- noop
--- noop
--- noop
--- addx 2
--- addx 6
--- noop
--- noop
--- noop
--- noop
--- noop
--- addx 1
--- noop
--- noop
--- addx 7
--- addx 1
--- noop
--- addx -13
--- addx 13
--- addx 7
--- noop
--- addx 1
--- addx -33
--- noop
--- noop
--- noop
--- addx 2
--- noop
--- noop
--- noop
--- addx 8
--- noop
--- addx -1
--- addx 2
--- addx 1
--- noop
--- addx 17
--- addx -9
--- addx 1
--- addx 1
--- addx -3
--- addx 11
--- noop
--- noop
--- addx 1
--- noop
--- addx 1
--- noop
--- noop
--- addx -13
--- addx -19
--- addx 1
--- addx 3
--- addx 26
--- addx -30
--- addx 12
--- addx -1
--- addx 3
--- addx 1
--- noop
--- noop
--- noop
--- addx -9
--- addx 18
--- addx 1
--- addx 2
--- noop
--- noop
--- addx 9
--- noop
--- noop
--- noop
--- addx -1
--- addx 2
--- addx -37
--- addx 1
--- addx 3
--- noop
--- addx 15
--- addx -21
--- addx 22
--- addx -6
--- addx 1
--- noop
--- addx 2
--- addx 1
--- noop
--- addx -10
--- noop
--- noop
--- addx 20
--- addx 1
--- addx 2
--- addx 2
--- addx -6
--- addx -11
--- noop
--- noop
--- noop$$ as input),
--- sample_input as (
--- select value as input, index as rn from sample_input_raw, table(split_to_table(input, '\n'))
---     ),
-source as (
-    select 'noop' as input, 0 as rn
-    union all 
-select input, rn from 
-    -- sample_data_a
-    INPUT_2022_10
-    -- sample_input
-),
-helper_step as (
-select row_number() over (order by null) as intra_step
-    from table(generator(rowcount => 3))
-),
-length_instruction as (
-select 'addx' as instruction, 2 as turns
+create or replace table temp_initial_items_11 as
+with source as (
+    select input, rn from INPUT_2022_11
     union all
-select 'noop' as instruction, 1 as turns
+    select null, (select max(rn) from INPUT_2022_11) + 1
 ),
-instructions as (
-select 
-    split_part(input, ' ',1) as instruction,
-    try_to_number(split_part(input, ' ',2)) as val,
-    input, 
-    rn
+parsed as (
+select *, 
+    regexp_substr_all(input, '\\d+') as vals,
+    case when input ilike 'monkey%' then vals[0]::number end as monkey,
+    case when input ilike '%starting%' then vals end as items,
+    case when input ilike '%operation%' then regexp_substr(input, '\\*|\\+') end as operator,
+    case when input ilike '%operation%' then vals[0] end as operation,
+    case when input ilike '%test%' then vals[0]::number end as divisible_by,
+    case when input ilike '%if true%' then vals[0]::number end as monkey_if_true,
+    case when input ilike '%if false%' then vals[0]::number end as monkey_if_false
 from source),
-steps as (
-    select
-        i.input,
-        i.instruction,
-        i.val,
-        i.rn,
-        li.turns,
-        h.intra_step,
-        row_number() over (order by i.rn, h.intra_step) - 1 as single_step
-    from instructions as i 
-    left join length_instruction as li 
-        on i.instruction = li.instruction
-    left join helper_step as h 
-        on h.intra_step <= li.turns
-    ),
-moves as (
-    select *,
-    case when single_step = 0 then 1 
-         when intra_step = turns then coalesce(val,0) 
-    else 0 end new_val,
-    sum(new_val) over (order by single_step rows between unbounded preceding and current row) as cum_sum
-
-    from steps
-    ),
-cycle_value as (
-    select *,
-    lag(cum_sum) over (order by single_step) as signal_during_cycle,
-    signal_during_cycle * single_step as strength,
-    -- During cycle  1: CRT draws pixel in position 0
-    mod(single_step - 1, 40) as pixel_position,
-    case when (pixel_position - 1 <= signal_during_cycle) and (signal_during_cycle <= pixel_position + 1) then '#'
-    else '.' end as pixel,
-    ceil(single_step / 40) as  row_
-    
-    from moves
+monkey_def as (
+select rn - 7 as group_monkey from source
+where input is null
 ),
-partii as (
-    select row_, listagg(pixel) as row_string
-    from cycle_value
-    where 1=1 and single_step != 0
+monkey_ass as (
+    select
+    source.rn,
+    max(md.group_monkey) as group_monkey
+from source
+    left join monkey_def as md
+    on md.group_monkey <= source.rn   
     group by 1
-    order by row_ asc
 )
-select sum(strength)::varchar as answer, 'part1' as part from cycle_value
-where single_step in (20,60,100,140,180,220)
-union all 
-select row_string as answer, 'part2' as part from partii;
+-- select * from monkey_ass
+-- order by rn;
+,
+tabular as (
+select
+    ma.group_monkey,
+    min(monkey) as monkey,
+    array_agg(items)[0] as items,
+    min(operator) as operator,
+    case when min(operation) is null then 'old' else min(operation) end as operation_value,
+    min(divisible_by) as divisible_by,
+    min(monkey_if_true) as monkey_if_true,
+    min(monkey_if_false) as monkey_if_false
+from parsed
+left join monkey_ass as ma
+    on ma.rn = parsed.rn
+group by ma.group_monkey
+order by monkey),
+stats as (
+    select count(*) as n_monkeys,
+        pow(10,sum(log(10,divisible_by)))::int as lcm
+    from tabular
+),
+items as (
+    select 
+        t.monkey as current_monkey,
+        it.value::number(38,0) as item,
+        0 as current_round
+    from tabular as t,lateral flatten(input => t.items) as it
+),
+cross_joined as (
+    select * exclude (items, group_monkey) from tabular
+    cross join stats
+)
+-- select * from items;
+-- select * from temp_initial_items_11;
+,
+rec_round as (
+    select 
+        item::number(38,0) as item,
+        current_monkey,
+        0 as next_monkey,
+        1 as next_round
+    from items
 
+    UNION ALL
+    
+    select 
+        case when rr.current_monkey = rr.next_monkey then 
+            (case 
+                when cj.operation_value = 'old' then rr.item * rr.item
+                when cj.operator = '*' then rr.item * cj.operation_value
+                when cj.operator = '+' then rr.item + cj.operation_value
+            end) % cj.lcm 
+            else rr.item end
+            as 
+            mod_item,
+        case when rr.current_monkey != rr.next_monkey then rr.current_monkey
+            else 
+                case when mod_item % cj.divisible_by = 0 then cj.monkey_if_true
+                else cj.monkey_if_false end
+            end as        current_monkey,
+        ((rr.next_monkey +1) % cj.n_monkeys) as mod_next_monkey,
+        case when mod_next_monkey = 0 then rr.next_round + 1 else rr.next_round end as next_round
+    
+    from rec_round as rr
+    left join cross_joined as cj 
+        on rr.current_monkey = cj.monkey
+    where rr.next_round <=10001
+    
+)
 
-select * from solved_2022_10;
-
--- "ANSWER","PART"
--- "12880","part1"
--- "####..##....##..##..###....##.###..####.","part2"
--- "#....#..#....#.#..#.#..#....#.#..#.#....","part2"
--- "###..#.......#.#..#.#..#....#.#..#.###..","part2"
--- "#....#.......#.####.###.....#.###..#....","part2"
--- "#....#..#.#..#.#..#.#....#..#.#.#..#....","part2"
--- "#.....##...##..#..#.#.....##..#..#.####.","part2"
+, summary as (
+    select next_monkey, count(*) as inspected
+    from rec_round
+    where next_monkey = current_monkey and next_round <= 10001
+    group by 1
+)
+select *
+from monkey_business
+order by inspected desc;
+-- select (120024 * 120022)::number(38,0); 
+-- 14399759920
+-- 18085004878 -- true
+-- 14405520528
