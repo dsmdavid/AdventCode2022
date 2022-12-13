352fb40b4ef9eb3d60a78e74174c12458ceff9eb
dsmdavid <61737063+dsmdavidIL@users.noreply.github.com>
2022-12-13 16:37:58 +0000

    switch to class

--- python/day_13.py
+++ python/day_13.py
@@ -62,42 +62,17 @@ def compare_two(one,two):
             if result != 0:
                 return result
         return 0
-# Use a merge_sort strategy
-def merge(left, right):
-    if len(left) == 0:
-        return right
-    if len(right) == 0:
-        return left
 
-    result = []
-    index_left = index_right = 0
-
-    while len(result) < len(left) + len(right):
-        if compare_two(left[index_left], right[index_right]) == 1:
-            result.append(left[index_left])
-            index_left += 1
-        else:
-            result.append(right[index_right])
-            index_right += 1
-        if index_right == len(right):
-            result += left[index_left:]
-            break
-
-        if index_left == len(left):
-            result += right[index_right:]
-            break
-
-    return result
-
-def merge_sort(array):
-    if len(array) < 2:
-        return array
-
-    middle = len(array) // 2
-
-    return merge(
-                left=merge_sort(array[:middle]),
-                right=merge_sort(array[middle:]))
+class Packet():
+    # I define compare_two as 1, 0, -1 so the lt, gt are mixed up!
+    def __init__(self, obj, *args):
+        self.obj = obj
+    def __lt__(self, other):
+        return compare_two(self.obj, other.obj) > 0
+    def __gt__(self, other):
+        return compare_two(self.obj, other.obj) < 0
+    def __eq__(self, other):
+        return compare_two(self.obj, other.obj) == 0
 
 
 if __name__ == "__main__":
@@ -121,11 +96,14 @@ if __name__ == "__main__":
     for v in pairs.values():
         l_pairs.extend(v)
     l_pairs.extend([DIVIDER_PACKET_1, DIVIDER_PACKET_2])
-    print(len(l_pairs))
-    m = merge_sort(l_pairs)
-    # for i, v in enumerate(m):
-        # print(i+1, '\t', v)
-    c1 = m.index(DIVIDER_PACKET_1) + 1
-    c2 = m.index(DIVIDER_PACKET_2) + 1
-    print(c1, c2, 'part2\t', c1*c2)
-  
+
+    p1 = Packet(DIVIDER_PACKET_1)
+    p2 = Packet(DIVIDER_PACKET_2)
+
+    packets_list = [Packet(i) for i in l_pairs]
+
+
+    packets_list.sort()
+    t1 = packets_list.index(p1) + 1
+    t2 = packets_list.index(p2) + 1
+    print(t1, t2, t1*t2)
